d6d09c2e7a8fbd1e28df4c2850ae7a8facfa3fab
JulianFernandez28 <fernandez.julian200@gmail.com>
2024-07-30 22:45:39 -0600

    fix bug in admob

--- admob-ads-in-flutter/android/app/src/main/kotlin/com/codelab/starter/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.codelab.starter
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
---
+++ admob_ads_influtter/android/app/src/main/kotlin/com/example/admob_ads_influtter/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.admob_ads_influtter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
